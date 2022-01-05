3ee94acfef97cbf16b6d14c586876b644af83b8b
Dwayzz <97143464+Dwayzz@users.noreply.github.com>
2022-01-04 22:03:44 -0600

    Add files via upload

---
+++ Name Search/Name Search/Form1.Designer.cs
@@ -0,0 +1,111 @@
+﻿namespace Name_Search
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.nameLabel = new System.Windows.Forms.Label();
+            this.nameTextBox = new System.Windows.Forms.TextBox();
+            this.checkButton = new System.Windows.Forms.Button();
+            this.exitButton = new System.Windows.Forms.Button();
+            this.popularityLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // nameLabel
+            // 
+            this.nameLabel.AutoSize = true;
+            this.nameLabel.Location = new System.Drawing.Point(40, 29);
+            this.nameLabel.Name = "nameLabel";
+            this.nameLabel.Size = new System.Drawing.Size(73, 13);
+            this.nameLabel.TabIndex = 0;
+            this.nameLabel.Text = "Enter a name:";
+            // 
+            // nameTextBox
+            // 
+            this.nameTextBox.Location = new System.Drawing.Point(119, 26);
+            this.nameTextBox.Name = "nameTextBox";
+            this.nameTextBox.Size = new System.Drawing.Size(91, 20);
+            this.nameTextBox.TabIndex = 1;
+            this.nameTextBox.TextChanged += new System.EventHandler(this.nameTextBox_TextChanged);
+            // 
+            // checkButton
+            // 
+            this.checkButton.Location = new System.Drawing.Point(230, 19);
+            this.checkButton.Name = "checkButton";
+            this.checkButton.Size = new System.Drawing.Size(173, 33);
+            this.checkButton.TabIndex = 2;
+            this.checkButton.Text = "Check Popularity";
+            this.checkButton.UseVisualStyleBackColor = true;
+            this.checkButton.Click += new System.EventHandler(this.checkButton_Click);
+            // 
+            // exitButton
+            // 
+            this.exitButton.Location = new System.Drawing.Point(699, 16);
+            this.exitButton.Name = "exitButton";
+            this.exitButton.Size = new System.Drawing.Size(73, 33);
+            this.exitButton.TabIndex = 4;
+            this.exitButton.Text = "Exit";
+            this.exitButton.UseVisualStyleBackColor = true;
+            this.exitButton.Click += new System.EventHandler(this.exitButton_Click);
+            // 
+            // popularityLabel
+            // 
+            this.popularityLabel.AutoSize = true;
+            this.popularityLabel.Location = new System.Drawing.Point(440, 26);
+            this.popularityLabel.Name = "popularityLabel";
+            this.popularityLabel.Size = new System.Drawing.Size(35, 13);
+            this.popularityLabel.TabIndex = 5;
+            this.popularityLabel.Text = "label1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 71);
+            this.Controls.Add(this.popularityLabel);
+            this.Controls.Add(this.exitButton);
+            this.Controls.Add(this.checkButton);
+            this.Controls.Add(this.nameTextBox);
+            this.Controls.Add(this.nameLabel);
+            this.Name = "Form1";
+            this.Text = "Name Search";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label nameLabel;
+        private System.Windows.Forms.TextBox nameTextBox;
+        private System.Windows.Forms.Button checkButton;
+        private System.Windows.Forms.Button exitButton;
+        private System.Windows.Forms.Label popularityLabel;
+    }
+}
+
---
+++ Name Search/Name Search/Form1.cs
@@ -0,0 +1,106 @@
+﻿//Dillon Waisner
+//11/9/2022
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Name_Search
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        private void checkButton_Click(object sender, EventArgs e)
+        {
+            //clear result label
+            popularityLabel.Text = "";
+
+            //try catch for file not found exception
+            try
+            {
+                //Empty input test
+                if (nameTextBox.Text == "")
+                {
+                    popularityLabel.Text = "No input provided";
+                }
+                else
+                {
+                    //Getting the names folder 
+                    string[] names = new string[400];
+                    int i = 0;
+                    string path = @"E:\ITSE 1330 C#\Homework Assignments\Lesson 7\Name Search\Name Search\bin\Debug\Names.txt";
+
+                    {
+                        string n = "";
+                        while ((s = sr.ReadLine()) != null)
+                        {
+                            names[i] = n;
+                            i++;
+                        }
+                    }
+                    bool check = false;
+                    //name and display result
+                    for (i = 0; i < names.Length; i++)
+                    {
+                        if (names[i] == nameTextBox.Text)
+                        {
+                            popularityLabel.Text = "This IS a popular name!";
+                            check = true;
+                            break;
+                        }
+                    }
+                    //If name isnt in list then text saying sorrt not pop name
+                    if (!check)
+                    {
+                        popularityLabel.Text = "Sorry, this IS NOT a popular name!";
+                    }
+                }
+                nameTextBox.Select();
+                }
+            //handling filenotfoundexceptions 
+                   catch (FileNotFoundException e)
+                {
+                popularityLabel.Text = ex.FileName + " " + "Sorry, NOT found!";
+            }
+        }
+    
+                
+
+                
+        
+
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+  
+            nameTextBox.Select();
+        }
+
+        private void exitButton_Click(object sender, EventArgs e)
+        {
+            //closes form
+            this.Close();
+        }
+
+        private void nameTextBox_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
